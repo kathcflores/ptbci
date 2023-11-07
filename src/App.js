02a12175f7a4555194db8af5d3567b5c8e9e7604
kathcflores <kathleen_c_flores@dlsu.edu.ph>
2023-11-07 12:33:21 +0800

    Added Topbar

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
-// function App() {
-//   return (
-//     <ColorModeContext.Provider value={colorMode}>
-//       <ThemeProvider theme={theme}>
-//         <CssBaseline />
-//         <div className="app">
-//           {shouldShowSidebar() && <Sidebar isSidebar={isSidebar} />}
-//           <main className="content">
-//             <Routes>
-//               <Route path="/" element={<Login />} />
-//               <Route path="/login" element={<Login />} />
-//               <Route path="/signup" element={<Signup />} />
-//               <Route path="/form" element={<Form />} />
-
-//               <Route path="/dashboard" element={<Dashboard />} />
-//               <Route path="/patient_info" element={<Patient_Info />} />
-//               <Route path="/contacts" element={<Contacts />} />
-//               <Route path="/symptoms" element={<Symptoms />} />
-//               <Route path="/form" element={<Form />} />
-//               <Route path="/bar" element={<Bar />} />
-//               <Route path="/pie" element={<Pie />} />
-//               <Route path="/line" element={<Line />} />
-//               <Route path="/calendar" element={<Calendar />} />
-//             </Routes>
-//           </main>
-//         </div>
-//       </ThemeProvider>
-//     </ColorModeContext.Provider>
-//   );
-// }
 import {useState} from 'react';
 import { useLocation } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Sidebar from "./pages/global/Sidebar";
+import Topbar from "./pages/global/Topbar";
 
 import { Outlet } from 'react-router-dom';
 
 function App() {
   const [theme, colorMode] = useMode();
-  const [isSidebar] = useState(true);
+  const [isSidebar, setIsSidebar] = useState(true);
   const location = useLocation();
 
   // Function to check if the current location matches certain paths
@@ -53,7 +24,10 @@ function App() {
         <CssBaseline />
         <div className="app">
           {shouldShowSidebar() && <Sidebar isSidebar={isSidebar} />}
+          <main className="content">
+          <Topbar setIsSidebar={setIsSidebar} />
           <Outlet />
+          </main>
         </div>
        </ThemeProvider>
      </ColorModeContext.Provider>
